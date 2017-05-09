fe9909e76f23c49ee6b5d152cf00f14fedc41833
Loic Lerat <loic.lerat@etu.utc.fr>
2017-05-09 14:52:45 +0200

    [Unity] Page connexion

--- Unity/Assets/Scripts/NetworkManager.cs
+++ Unity/Assets/Scripts/NetworkManager.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System;
 using System.Text;
+using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviour
 {
 
-    public bool isAtStartup = true;
+    public bool serverOn = false;
     List<NetworkClient> myClient;
+
+	// UI elements
+	public Text serverStatus;
+	public Text players;
+
 
     private void Awake()
     {
@@ -18,36 +24,26 @@ public class NetworkManager : MonoBehaviour
 
     void Update()
     {
-        if (isAtStartup)
-        {
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                SetupServer();
-            }
-        }
-    }
 
-    void OnGUI()
-    {
-        if (isAtStartup)
-        {
-            GUI.Label(new Rect(2, 10, 150, 100), "Appuyer S pour démarrer le serveur");
-
-        }
     }
 
     // Create a server and listen on a port
     public void SetupServer()
     {
-        isAtStartup = false;
-        NetworkServer.SetNetworkConnectionClass<MyConnection>();
-        NetworkServer.useWebSockets = true;
-        if (NetworkServer.Listen(1337))
-            Debug.Log("Ecoute localhost:1337");
-        else
-            Debug.Log("Couldn't open server :/");
-        NetworkServer.RegisterHandler(MsgType.Connect, OnNewClient);
+		if (!serverOn) {
+			NetworkServer.SetNetworkConnectionClass<MyConnection> ();
+			NetworkServer.useWebSockets = true;
+			if (NetworkServer.Listen (1337)) {
+				serverOn = true;
+				Debug.Log ("Ecoute localhost:1337");
 
+				// Changing server status displayed state
+				serverStatus.color = Color.green;
+				serverStatus.text = "On";
+			} else
+				Debug.Log ("Couldn't open server :/");
+			NetworkServer.RegisterHandler (MsgType.Connect, OnNewClient);
+		}
     }
 
     // client function
