93bceb2ff8d8229a57585533386a54941c3ac5fc
Benjamin <benjamin.roelandt@gmail.com>
2017-05-13 04:13:28 +0200

    quelques corrections dans les animations, mais des trucs bug toujours

--- Unity/Assets/Scripts/Player.cs
+++ Unity/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -10,11 +11,14 @@ public class Player : MonoBehaviour {
     public bool Connected { get; set; }
     public string Key { get; set; }
     public Color Coloration { get { return sprite.color; } set { sprite.color = value; } }
+    public bool IsGrounded { get; private set; }
 
     private Rigidbody2D body;
     private SpriteRenderer sprite;
-    private Animation anim;
-
+    private Animator anim;
+    public  LayerMask groundMask;
+    private Transform groundCheck;    // A position marking where to check if the player is grounded.
+    const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 
     // Test things
     [Header("Debug")]
@@ -29,39 +33,60 @@ public class Player : MonoBehaviour {
 
     [Header("Player tuning")]
     public int jumpCount = 2;
-    private int remainingJump = 2;
+    public int remainingJump = 2;
     public float moveSpeed = 5;
     public float jumpForce = 1.2f;
-    enum Direction { None, Left, Right }
-    private Direction dir = Direction.None;
+    enum Direction { None, Left, Right, Down, Up }
+    private Direction wantedDirection = Direction.None;
 
-	// Use this for initialization
-	void Awake () {
+    // Animation
+    private const string kStandAnim = "Standing";
+    private const string kWalkAnim = "Walking";
+    private const string kJumpAnim = "Jumping";
+    private const string kFallAnim = "Jumping";
+    private const string kCrouchAnim = "Crouching";
+
+    
+    // Use this for initialization
+    void Awake () {
         sprite = GetComponent<SpriteRenderer>();
         body = GetComponent<Rigidbody2D>();
-        // anim = GetComponent<Animation>();
-       
+        anim = GetComponent<Animator>();
+
+        groundCheck = transform.Find("GroundCheck");
+        //m_CeilingCheck = transform.Find("CeilingCheck");
     }
 
-    public bool IsGrounded()
+    // Jump input must treated here for some reasons ...
+    private void Update()
     {
-        return Physics2D.Raycast(body.position, -Vector2.up, 0.1f);
+        if (enableKeyboard)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Jump();
+        }
     }
-
     // Update is called once per frame
     void FixedUpdate ()
     {
+        CheckGrounded();
+        UpdateAnimation();
+
+    //    body.isKinematic = IsGrounded && wantedDirection == Direction.None;
+
         // input test
-        if(enableKeyboard)
+        if (enableKeyboard)
         {
             if (Input.GetKeyDown(KeyCode.Space))
                 Jump();
             if (Input.GetKey(KeyCode.Q))
-                dir = Direction.Left;
-            else if (Input.GetKey (KeyCode.D))
-                dir = Direction.Right;
+                wantedDirection = Direction.Left;
+            else if (Input.GetKey(KeyCode.D))
+                wantedDirection = Direction.Right;
+            else if (Input.GetKey(KeyCode.S))
+                wantedDirection = Direction.Down;
             else
-                dir = Direction.None;
+                wantedDirection = Direction.None;
         }
 
         if (singleAction || continousAction)
@@ -90,17 +115,63 @@ public class Player : MonoBehaviour {
         }
         // end test
 
-        if(dir != Direction.None)
+        if(wantedDirection == Direction.Left || wantedDirection == Direction.Right)
         {
-            Vector2 velocity = new Vector2((dir == Direction.Left ? -1 : 1) * moveSpeed, body.velocity.y);
+            Vector2 velocity = new Vector2((wantedDirection == Direction.Left ? -1 : 1) * moveSpeed, body.velocity.y);
             body.velocity = velocity;
         }
-
-        if(IsGrounded())
-        {
-            remainingJump = jumpCount;
-        }
 	}
+
+    private void CheckGrounded()
+    {
+        IsGrounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, groundMask);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                IsGrounded = true;
+        }
+
+        if (IsGrounded)
+            remainingJump = jumpCount;
+    }
+
+    private void UpdateAnimation()
+    {
+        if (wantedDirection == Direction.Left && IsGrounded)
+        {
+            sprite.flipX = true;
+            anim.Play(kWalkAnim);
+        }
+        else if(wantedDirection == Direction.Right && IsGrounded)
+        {
+            sprite.flipX = false;
+            anim.Play(kWalkAnim);
+        }
+        else if (wantedDirection == Direction.None && IsGrounded)
+        {
+            anim.Play(kStandAnim);
+        }
+        else if (wantedDirection == Direction.Down && IsGrounded)
+        {
+            anim.Play(kCrouchAnim);
+        }
+        else if(!IsGrounded)
+        {
+            if (body.velocity.y > 0)
+                anim.Play(kJumpAnim);
+            else
+                anim.Play(kStandAnim);
+
+            if (wantedDirection == Direction.Left)
+                sprite.flipX = true;
+            else if (wantedDirection == Direction.Left)
+                sprite.flipX = false;
+        }
+    }
 
     public void ReceiveMessage(Message mess)
     {
@@ -114,12 +185,16 @@ public class Player : MonoBehaviour {
                     Jump();
                     break;
                 case Buttons.DPadLeft:
-                    sprite.flipX = false;
-                    dir = Direction.Left;
+                    wantedDirection = Direction.Left;
                     break;
                 case Buttons.DPadRight:
-                    sprite.flipX = true;
-                    dir = Direction.Right;
+                    wantedDirection = Direction.Right;
+                    break;
+                case Buttons.DPadDown:
+                    wantedDirection = Direction.Down;
+                    break;
+                case Buttons.DPadUp:
+                    wantedDirection = Direction.Up;
                     break;
             }
         }
@@ -131,7 +206,9 @@ public class Player : MonoBehaviour {
             {
                 case Buttons.DPadLeft:
                 case Buttons.DPadRight:
-                    dir = Direction.None;
+                case Buttons.DPadDown:
+                case Buttons.DPadUp:
+                    wantedDirection = Direction.None;
                     break;
             }
 
@@ -142,9 +219,10 @@ public class Player : MonoBehaviour {
     {
         if (remainingJump <= 0)
             return;
-        if(!IsGrounded())
-            body.velocity = new Vector2();
-        body.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        if (!IsGrounded)
+            body.velocity = Vector2.zero;
+
+        body.AddForce(new Vector2(0f, jumpForce));
         --remainingJump;
     }
 }
