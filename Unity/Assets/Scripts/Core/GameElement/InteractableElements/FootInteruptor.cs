af34edca71668b8a2f149ace946082420228ccfb
Benjamin <benjamin.roelandt@gmail.com>
2017-05-25 19:55:24 +0200

    ajout interupteurs

---
+++ Unity/Assets/Scripts/Core/GameElement/InteractableElements/FootInteruptor.cs
@@ -0,0 +1,54 @@
+﻿using CreativeSpore.SuperTilemapEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using System;
+
+[RequireComponent(typeof(TileData))]
+public class FootInteruptor : InteractableElement
+{
+    public enum eInteruptorState { Up = 0, Down = 1};
+    private eInteruptorState currentState;
+    private TileData tiledata;
+    public List<uint> TileId = new List<uint>();
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        tiledata = GetComponent<TileData>();
+        Assert.IsNotNull(tiledata, "A FootInteruptor require a TileData component!");
+        currentState = (eInteruptorState)tiledata.Tile.paramContainer.GetIntParam("state");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void PlayerTriggerEnter(Player p)
+    {
+        SetState(eInteruptorState.Down);
+        NotifyController(p);
+    }
+
+    protected override void PlayerTriggerExit(Player p)
+    {
+        SetState(eInteruptorState.Up);
+        NotifyController(p);
+    }
+
+    private void SetState(eInteruptorState state)
+    {
+        currentState = state;
+        tiledata.Map.SetTileData(tiledata.Position, TileId[(int)currentState]);
+        tiledata.Map.UpdateMesh();
+    }
+
+    public eInteruptorState CurrentState()
+    {
+        return currentState;
+    }
+}
