75dcca1539af581380e47b2d3caf0a0aaec730a2
Benjamin <benjamin.roelandt@gmail.com>
2017-06-10 18:18:53 +0200

    Paufinage Interface + Ecran de fin

---
+++ Unity/Assets/Scripts/Enigmas/_Common/FinalDoor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoor : InteractableElementBehaviour {
+
+    public override void OnInteraction(InteractableElement ie, Player p)
+    {
+        Utils.LoadScene("Scene/EnigmaClear");
+    }
+}
